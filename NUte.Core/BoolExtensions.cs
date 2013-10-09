0c5c1f860e804572ca4915bf40dcc1b103789752
synariasol <info@synaria.com>
2013-10-09 20:33:23 +1100

    Added extension method to return bool string in lower case

---
+++ NUte.Core/BoolExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace NUte
+{
+    public static class BoolExtensions
+    {
+         public static string ToLowerString(this bool value)
+         {
+             var result = value.ToString();
+
+             return result.ToLower();
+         }
+    }
+}
