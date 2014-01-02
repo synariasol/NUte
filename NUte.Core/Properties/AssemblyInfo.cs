e1982ad7b1f6f759c7fe6166658fdc11bd996b69
synariasol <info@synaria.com>
2014-01-02 11:01:03 +1100

    Updated NuGet spec file and versioning

--- NUte.Core/Properties/AssemblyInfo.cs
+++ NUte.Core/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("64c1630e-dd7b-4073-94c2-4f1edf85cb6d")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
